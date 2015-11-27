e21c16ab4cae67813944da0532569ee45f17589b
David Lawrence Miller <github@ninepointeightone.net>
2015-11-27 12:01:41 -0500

    working geom_emoji for ggplot2 1.0.1.9003?

--- R/geom_emoji.R
+++ R/geom_emoji.R
@@ -20,42 +20,82 @@ emojisGrob <- function(x, y, size, emoji){
               width         = size)
 }
 
-GeomEmoji <- proto(ggplot2:::Geom, {
+GeomEmoji <- ggproto("GeomEmoji", Geom,
+#  objname = "emoji",
+#  desc = "Emojis!!!",
 
-  objname <- "emoji"
-  desc <- "Emojis!!!"
+  #draw_panel = function(., data, scales, coordinates, ...) {
+  draw_panel = function(data, panel_scales, coord, na.rm = FALSE) {
+    #coords = coord_transform(coordinates, data, scales)
+    coords = coord$transform(data, panel_scales)
+    ggplot2:::ggname("geom_emoji",
+      emojisGrob(coords$x, coords$y, coords$size, coords$emoji)
+    )
+  },
 
-  draw <- function(., data, scales, coordinates, ...) {
-    coords <- coord_transform(coordinates, data, scales)
-    emojisGrob(coords$x, coords$y, coords$size, coords$emoji)
-  }
+#  draw_legend = function(., data, ...) {
+#    data = aesdefaults(data, .$default_aes(), list(...))
+#
+#    with(data, ggname(.$my_name(),
+#         emojisGrob(0.05, 0.05, size, emoji)))
+#  },
 
-  draw_legend <- function(., data, ...) {
-    data <- aesdefaults(data, .$default_aes(), list(...))
+#  default_stat = function(.) StatIdentity,
+  non_missing_aes = c("emoji", "size"),
+  required_aes = c("x", "y"),
+  default_aes = aes(size=0.05, emoji="1f697"),
+#  guide_geom = "emoji",
 
-    with(data, ggname(.$my_name(),
-         emojisGrob(0.05, 0.05, size, emoji)))
-  }
+  icon = function(.){}, # a grob representing the geom for the webpage
 
-  default_stat <- function(.) StatIdentity
-  required_aes <- c("x", "y")
-  default_aes <- function(.) aes(size=0.05, emoji="1f697")
-  guide_geom <- function(.) "emoji"
+  desc_params = list( # description of the (optional) parameters of draw
+     ),
 
-  icon <- function(.) # a grob representing the geom for the webpage
-
-  desc_params <- list( # description of the (optional) parameters of draw
-     )
-
-  seealso <- list(
+  seealso = list(
     geom_point = GeomPoint$desc
-  )
+  ),
 
-  examples <- function(.) {
+  examples = function(.) {
     # examples of the geom in use
   }
+)
 
-})
+#GeomEmoji <- proto(ggplot2:::Geom, {
+#
+#  objname <- "emoji"
+#  desc <- "Emojis!!!"
+#
+#  draw <- function(., data, scales, coordinates, ...) {
+#    coords <- coord_transform(coordinates, data, scales)
+#    emojisGrob(coords$x, coords$y, coords$size, coords$emoji)
+#  }
+#
+#  draw_legend <- function(., data, ...) {
+#    data <- aesdefaults(data, .$default_aes(), list(...))
+#
+#    with(data, ggname(.$my_name(),
+#         emojisGrob(0.05, 0.05, size, emoji)))
+#  }
+#
+#  default_stat <- function(.) StatIdentity
+#  required_aes <- c("x", "y")
+#  default_aes <- function(.) aes(size=0.05, emoji="1f697")
+#  guide_geom <- function(.) "emoji"
+#
+#  icon <- function(.) # a grob representing the geom for the webpage
+#
+#  desc_params <- list( # description of the (optional) parameters of draw
+#     )
+#
+#  seealso <- list(
+#    geom_point = GeomPoint$desc
+#  )
+#
+#  examples <- function(.) {
+#    # examples of the geom in use
+#  }
+#
+#})
 
 
 geom_emoji <- function(mapping = NULL, data = NULL, stat = "identity",
